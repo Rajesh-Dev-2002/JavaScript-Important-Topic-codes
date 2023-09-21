42f5932e19116854a241084dd44d22d12cd0e93e
Rajesh Kumar Mohanta <119045660+Rajesh7853814223@users.noreply.github.com>
2023-09-21 12:42:00 +0530

    Create functions-in-js.js

---
+++ functions-in-js.js
@@ -0,0 +1,17 @@
+function one() {
+    console.log("f-1");
+}
+function two() {
+    console.log("f-2");
+}
+function three() {
+    console.log("f-3");
+}
+
+
+// one()
+setTimeout(one,2000)
+
+three()
+
+two()
